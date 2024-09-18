2e844c75a3e6e6d5973061e098be48255fa33651
Benjamin-dhoe <117100231+Benjamin-dhoe@users.noreply.github.com>
2024-09-18 12:51:27 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -139,7 +139,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const userNavHtml = `
         <div class="navlink logged w-inline-block">
             <img src="/images/usericon.svg" loading="lazy" alt="" class="loggedicon">
-            <div class="rotatedtext">&gt;</div>
+            <div>&gt;</div>
         </div>
         `;
         document.querySelector('.navlinksholder').insertAdjacentHTML('beforeend', userNavHtml);
