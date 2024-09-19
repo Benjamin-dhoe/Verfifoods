074affc2d1acb661744862591ddbb981de22e7df
Benjamin-dhoe <117100231+Benjamin-dhoe@users.noreply.github.com>
2024-09-19 11:48:29 +0200

    Update dash-aanbod.js

--- js/dash-aanbod.js
+++ js/dash-aanbod.js
@@ -11,3 +11,22 @@ document.querySelectorAll('[closesidemenu]').forEach(function(element) {
         document.querySelector('.holderappdashboard').style.display = 'block';
     });
   });
+
+document.querySelectorAll('[openproducts]').forEach(element => {
+    element.addEventListener('click', () => {
+      const openProductsValue = element.getAttribute('openproducts');
+      
+      const targetElement = document.querySelector(`[products="${openProductsValue}"]`);
+        
+    if (window.innerWidth < 767) {
+        document.querySelector('.holderlinksdashboard').style.display = 'none';
+        document.querySelector('.holderappdashboard').style.display = 'block';
+      }
+        
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+      } else {
+        console.log(`No element found with products="${openProductsValue}"`);
+      }
+    });
+  });
