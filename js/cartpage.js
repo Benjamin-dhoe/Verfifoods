e192d6ae3cb6cfcfdf6b708895e32ca332ce320c
Benjamin-dhoe <117100231+Benjamin-dhoe@users.noreply.github.com>
2024-09-21 11:44:53 +0200

    Update cartpage.js

--- js/cartpage.js
+++ js/cartpage.js
@@ -71,20 +71,7 @@ async function displayCartItems() {
                         <div class="spacer10px"></div>
                         <div class="alwaysflexhorizontal">
                             <input class="w-input qtyinput" type="number" value="${quantity}" min="1">
-                            <div class="deletebtn w-embed">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="14" viewbox="0 0 12 14">
-                                    <g id="_1814090_delete_garbage_trash_icon_1_" data-name="1814090_delete_garbage_trash_icon (1)" transform="translate(-12.5 -8)">
-                                        <g id="Icon-Trash" transform="translate(12.5 8)">
-                                            <path id="Fill-6" d="M-207.5-205.1h1.471v11.77H-207.5Z" transform="translate(213.385 212.407)" fill="currentColor"></path>
-                                            <path id="Fill-7" d="M-201.5-205.1h1.471v11.77H-201.5Z" transform="translate(210.327 212.407)" fill="currentColor"></path>
-                                            <path id="Fill-8" d="M-195.5-205.1h1.471v11.77H-195.5Z" transform="translate(207.27 212.407)" fill="currentColor"></path>
-                                            <path id="Fill-9" d="M-219.5-214.1h19.126v1.471H-219.5Z" transform="translate(219.5 216.994)" fill="currentColor"></path>
-                                            <path id="Fill-10" d="M-200.142-216.371h-1.373v-1.471a.775.775,0,0,0-.785-.785h-2.942a.775.775,0,0,0-.785.785v1.471H-207.4v-1.471A2.177,2.177,0,0,1-205.242-220h2.942a2.177,2.177,0,0,1,2.158,2.158v1.471" transform="translate(213.334 220)" fill="currentColor"></path>
-                                            <path id="Fill-11" d="M-203.995-192.789h-8.827a2.381,2.381,0,0,1-2.3-2.158L-216.5-212.6l1.471-.1,1.373,17.655a.889.889,0,0,0,.834.785h8.827a.852.852,0,0,0,.834-.785l1.373-17.655,1.471.1-1.373,17.655a2.342,2.342,0,0,1-2.3,2.158" transform="translate(217.971 216.28)" fill="currentColor"></path>
-                                        </g>
-                                    </g>
-                                </svg>
-                            </div>
+                            <img class="deleteicon" src="/images/deleteicon.svg" loading="lazy">
                         </div>
                     </div>
                     <div class="winkelmanditempriceholder">
@@ -112,22 +99,24 @@ function setupCartEventListeners() {
             const productId = event.target.closest('.winkemanditem').getAttribute('data-product-id');
             const newQuantity = parseInt(event.target.value);
 
-            // Update the cart in localStorage
-            updateCartQuantity(productId, newQuantity);
+            if (!isNaN(newQuantity) && newQuantity > 0) {
+                // Update the cart in localStorage if valid
+                updateCartQuantity(productId, newQuantity);
+            } else if (newQuantity === 0) {
+                // Optionally, do not remove the product immediately; you can add a confirmation or just ignore
+                event.target.value = ''; // Clear the input field for user to re-enter a value
+            }
+            
             // Recalculate the total price
             recalculateTotalPrice();
         });
     });
 
     // Delete button event listener
-    document.querySelectorAll('.deletebtn').forEach(button => {
-        button.addEventListener('click', (event) => {
+    document.querySelectorAll('.deleteicon').forEach(icon => {
+        icon.addEventListener('click', (event) => {
             const productId = event.target.closest('.winkemanditem').getAttribute('data-product-id');
-
-            // Remove item from cart
-            removeFromCart(productId);
-            // Recalculate the total price
-            recalculateTotalPrice();
+            removeFromCart(productId); // Remove item from cart
         });
     });
 }
@@ -135,7 +124,7 @@ function setupCartEventListeners() {
 // Function to update the cart quantity in localStorage
 function updateCartQuantity(productId, newQuantity) {
     const cart = JSON.parse(localStorage.getItem('cart')) || {};
-
+    
     if (newQuantity > 0) {
         cart[productId] = newQuantity;
     } else {
@@ -159,16 +148,19 @@ function recalculateTotalPrice() {
     const cart = JSON.parse(localStorage.getItem('cart')) || {};
     let totalPrice = 0;
 
-    Object.keys(cart).forEach(async (productId) => {
+    const pricePromises = Object.keys(cart).map(async (productId) => {
         const productInfo = await getProductFromFirestore(productId);
         if (productInfo) {
             totalPrice += productInfo.prijs * cart[productId];
         }
     });
 
-    document.getElementById('totalprice').textContent = `${totalPrice.toFixed(2)} €`;
+    Promise.all(pricePromises).then(() => {
+        document.getElementById('totalprice').textContent = `${totalPrice.toFixed(2)} €`;
+    });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     displayCartItems();
 });
+
