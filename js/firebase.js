04252ffc3d904d5e6e8464930a37017518b40391
Benjamin-dhoe <117100231+Benjamin-dhoe@users.noreply.github.com>
2024-09-26 22:12:23 +0200

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp, getApps, getApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCIQlfqz-Hd-Oe0tNjnEfJdwHwMy3JuNr4",
+    authDomain: "test-9efbe.firebaseapp.com",
+    projectId: "test-9efbe",
+    storageBucket: "test-9efbe.appspot.com",
+    messagingSenderId: "218327249026",
+    appId: "1:218327249026:web:0d48fa588ac45ef557049b",
+    measurementId: "G-2ZTZCL27TE"
+};
+
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
