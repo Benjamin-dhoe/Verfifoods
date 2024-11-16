be9d00bc7e9d26af20c2be1be50d47f1e996a45c
Benjamin-dhoe <117100231+Benjamin-dhoe@users.noreply.github.com>
2024-11-16 01:18:51 +0100

    Update cartpage.js

--- js/cartpage.js
+++ js/cartpage.js
@@ -159,17 +159,6 @@ document.addEventListener('DOMContentLoaded', () => {
     displayCartItems();
 });
 
-function getCookie(name) {
-        const nameEQ = name + "=";
-        const ca = document.cookie.split(';');
-        for (let i = 0; i < ca.length; i++) {
-            let c = ca[i];
-            while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-            if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-        }
-        return null;
-    }
-
 // Function to handle the order button click
 async function handleOrderButtonClick() {
     const tokencookie = getCookie('token');
