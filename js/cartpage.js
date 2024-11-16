016b6174b7f044a4731998ca423eb2a376da190e
Benjamin-dhoe <117100231+Benjamin-dhoe@users.noreply.github.com>
2024-11-16 01:17:15 +0100

    Update cartpage.js

--- js/cartpage.js
+++ js/cartpage.js
@@ -12,12 +12,24 @@ function hideLoadingSpinner() {
     loadingSpinner.style.display = 'none';
 }
 
+function getCookie(name) {
+        const nameEQ = name + "=";
+        const ca = document.cookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+        }
+        return null;
+    }
+
 // Fetch cart details from Cloud Function
 async function fetchCartDetailsFromCloudFunction(cart) {
     try {
-
+        const token = getCookie('token');
         // Prepare payload
         const payload = {
+            token,
             cart
         };
 
