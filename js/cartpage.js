e36b766f4869cfcee5d5fa9bd40a3dd5c07eb4b6
Benjamin-dhoe <117100231+Benjamin-dhoe@users.noreply.github.com>
2024-11-16 01:44:41 +0100

    Update cartpage.js

--- js/cartpage.js
+++ js/cartpage.js
@@ -34,7 +34,13 @@ async function fetchCartDetailsFromCloudFunction(cart) {
         };
 
         // Fetch cart details
-        const response = await fetch(cloudFunctionURL);
+        const response = await fetch(cloudFunctionURL, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(payload),
+        });
 
         if (!response.ok) {
             throw new Error(`Error fetching cart details: ${response.statusText}`);
